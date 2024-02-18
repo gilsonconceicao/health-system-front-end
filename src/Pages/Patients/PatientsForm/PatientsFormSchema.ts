af3d4b4fd4f8d4b30b1dffaa87ff61ddb9f50527
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-02-17 21:24:12 -0300

    remove requireds fields

--- src/Pages/Patients/PatientsForm/PatientsFormSchema.ts
+++ src/Pages/Patients/PatientsForm/PatientsFormSchema.ts
@@ -8,11 +8,11 @@ export const contactPatientsFormSchema = yup.object({
   phoneNumber: yup.string().required("Telefone é obrigatório").typeError("Campo inválido"),
   birthDate: yup.string().required("Data de nascimento é obrigatório").typeError("Campo inválido"),
   address: yup.object().shape({
-    street: yup.string().required("Rua é obrigatório").typeError("Campo inválido"),
-    state: yup.string().required("Estado é obrigatório").typeError("Campo inválido"),
-    zipCode: yup.string().required("CEP é obrigatório").typeError("Campo inválido"),
-    city: yup.string().required("Cidade é obrigatório").typeError("Campo inválido"),
-    number: yup.string().required("Número é obrigatório").typeError("Campo inválido"),
+    street: yup.string().nullable(),
+    state: yup.string().nullable(),
+    zipCode: yup.string().nullable(),
+    city: yup.string().nullable(),
+    number: yup.string().nullable()
   })
 });
 
